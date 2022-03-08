cac8a74625f6f290a3130fd5c5a1091d468bf526
fhkhd <fhkhdkri@yahoo.com>
2022-03-08 04:43:23 +0330

    create listdevice method

--- app/src/main/java/com/example/bluetoothconnection/MainActivity.java
+++ app/src/main/java/com/example/bluetoothconnection/MainActivity.java
@@ -5,25 +5,30 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothServerSocket;
 import android.bluetooth.BluetoothSocket;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
+import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.util.Set;
 import java.util.UUID;
 
 public class MainActivity extends AppCompatActivity {
 
     private Button lisen_btn , send_btn , deviseslist_btn ;
     private EditText mssg_ed;
-    private TextView connection_mode_btn , txt_message;
-    private RecyclerView recycler_devices;
+    private TextView connection_mode_btn , txt_message , nofind_tv;
+    private ListView listView_devices;
 
 
     private BluetoothAdapter bluetoothAdapter;
@@ -58,12 +63,14 @@ public class MainActivity extends AppCompatActivity {
         mssg_ed = findViewById(R.id.mssg_ed);
         connection_mode_btn = findViewById(R.id.connection_mode_btn);
         txt_message = findViewById(R.id.txt_message);
-        recycler_devices = findViewById(R.id.recycler_devices);
+        listView_devices = findViewById(R.id.listView_devices);
+        nofind_tv = findViewById(R.id.nofind_tv);
     }
 
     @Override
     protected void onResume() {
         super.onResume();
+        listDevice();
     }
 
     private class SendReceive extends Thread
@@ -147,5 +154,111 @@ public class MainActivity extends AppCompatActivity {
             return true;
         }
     });
+
+    private class ClientClass extends Thread
+    {
+        private BluetoothDevice device;
+        private BluetoothSocket socket;
+
+        public ClientClass (BluetoothDevice device1)
+        {
+            device=device1;
+
+            try {
+                socket=device.createRfcommSocketToServiceRecord(MY_UUID);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        public void run()
+        {
+            try {
+                socket.connect();
+                Message message=Message.obtain();
+                message.what=STATE_CONNECTED;
+                handler.sendMessage(message);
+
+                sendReceive=new SendReceive(socket);
+                sendReceive.start();
+
+            } catch (IOException e) {
+                e.printStackTrace();
+                Message message=Message.obtain();
+                message.what=STATE_CONNECTION_FAILED;
+                handler.sendMessage(message);
+            }
+        }
+    }
+
+    private class ServerClass extends Thread
+    {
+        private BluetoothServerSocket serverSocket;
+
+        public ServerClass(){
+            try {
+                serverSocket=bluetoothAdapter.listenUsingRfcommWithServiceRecord(APP_NAME,MY_UUID);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        public void run()
+        {
+            BluetoothSocket socket=null;
+
+            while (socket==null)
+            {
+                try {
+                    Message message=Message.obtain();
+                    message.what=STATE_CONNECTING;
+                    handler.sendMessage(message);
+
+                    socket=serverSocket.accept();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                    Message message=Message.obtain();
+                    message.what=STATE_CONNECTION_FAILED;
+                    handler.sendMessage(message);
+                }
+
+                if(socket!=null)
+                {
+                    Message message=Message.obtain();
+                    message.what=STATE_CONNECTED;
+                    handler.sendMessage(message);
+
+                    sendReceive=new SendReceive(socket);
+                    sendReceive.start();
+                    break;
+                }
+            }
+        }
+    }
+
+
+    private void listDevice() {
+
+        Set<BluetoothDevice> bt=bluetoothAdapter.getBondedDevices();
+        String[] strings=new String[bt.size()];
+        btArray=new BluetoothDevice[bt.size()];
+        int index=0;
+
+        if( bt.size()>0)
+        {
+            nofind_tv.setVisibility(View.GONE);
+
+            for(BluetoothDevice device : bt)
+            {
+                btArray[index]= device;
+                strings[index]=device.getName();
+                index++;
+            }
+            ArrayAdapter<String> arrayAdapter=new ArrayAdapter<String>(getApplicationContext(),android.R.layout.simple_list_item_1,strings);
+            listView_devices.setAdapter(arrayAdapter);
+        }
+    }
+
+
 
 }
